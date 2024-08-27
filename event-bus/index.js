81070494f878b5811aa3bbf7db3b7559be1f97d1
guille garcia <ggarciacardiel@gmail.com>
2024-08-27 20:19:44 +0200

    updates event-bus queries from localhost to services

--- event-bus/index.js
+++ event-bus/index.js
@@ -19,19 +19,19 @@ app.post("/events", (req, res) => {
   });
 
   // Comments service
-  /* axios.post("http://localhost:4001/events", event).catch((err) => {
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
     console.log(err.message);
-  }); */
+  });
 
   // Query service
-  /* axios.post("http://localhost:4002/events", event).catch((err) => {
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
     console.log(err.message);
-  }); */
+  });
 
   // Moderation service
-  /* axios.post("http://localhost:4003/events", event).catch((err) => {
+  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
     console.log(err.message);
-  }) */;
+  }) 
 
   res.send({ status: "OK" });
 });
